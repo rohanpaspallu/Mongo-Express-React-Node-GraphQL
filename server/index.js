aa07dc9478c945d678f0e0b2234c665c31050b97
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2023-12-21 06:34:35 -0500

    Bringing some external static data to start with GraphQL

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 const port = process.env.port || 5000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
