71da3bd2a609e14036043748803d628417e16626
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-01-03 03:23:15 -0500

    Created some new components and included ApolloServer functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import Clients from "./components/Clients";
+import Header from "./components/Header";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
   );
 }
 
