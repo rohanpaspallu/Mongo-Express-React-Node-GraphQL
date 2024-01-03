905d895a1e22d2a752b7e6a60378b7940e4f67f1
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-01-03 03:45:32 -0500

    Created a simple list of all clients data fetched from graphql server

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
-
-const GET_CLIENTS = gql`
-  query getClients {
-    clients {
-      name
-      email
-      phone
-    }
-  }
-`;
+import ClientRow from "./ClientRow";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import Spinner from "./Spinner";
 
 const Clients = () => {
   const { loading, error, data } = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>Loading</p>;
+  if (loading) return <Spinner />;
   if (error) return <p>Something went wrong</p>;
-  return <>{!loading && !error && <h1>Clients</h1>}</>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>email</th>
+              <th>phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientRow client={client} key={client.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 
 export default Clients;
