7222280f6601a815e4302143f8fcc9420f71e241
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-01-03 03:32:37 -0500

    Added some important code to get the data out from graphql

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CLIENTS = gql`
+  query getClients {
+    clients {
+      name
+      email
+      phone
+    }
+  }
+`;
 
 const Clients = () => {
-  return <div>Clients</div>;
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Something went wrong</p>;
+  return <>{!loading && !error && <h1>Clients</h1>}</>;
 };
 
 export default Clients;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
@@ -8,6 +9,7 @@ const app = express();
 const port = process.env.port || 5000;
 
 connectDB();
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
