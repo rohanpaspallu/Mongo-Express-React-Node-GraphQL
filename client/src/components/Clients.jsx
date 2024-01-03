ba7c2835037cd0ddbd6c38c17e538eea5c70329e
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-01-03 05:25:52 -0500

    Deleting the client from the list

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import ClientRow from "./ClientRow";
 import { GET_CLIENTS } from "../queries/clientQueries";
 import Spinner from "./Spinner";
--- client/src/queries/clientQueries.js
+++ client/src/queries/clientQueries.js
@@ -2,6 +2,7 @@ import { gql } from "@apollo/client";
 const GET_CLIENTS = gql`
   query getClients {
     clients {
+      id
       name
       email
       phone
