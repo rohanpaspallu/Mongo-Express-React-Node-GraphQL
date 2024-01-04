0c16149bd71c556acc501093f668c044c27b1105
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-01-04 03:36:29 -0500

    removed a bunch of errors and added the feature of adding client in the database using apollo client

--- client/src/components/AddClientsModal.jsx
+++ client/src/components/AddClientsModal.jsx
@@ -1,15 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FaUser } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
 import {} from "@apollo/client";
-
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 const AddClientsModal = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: [...clients, addClient],
+        },
+      });
+    },
+  });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(email, name, phone);
+    if (name === "" || email === "" || phone === "")
+      return alert("please fill in all the fields");
+
+    addClient(name, email, phone);
+    setName("");
+    setEmail("");
+    setPhone("");
   };
   return (
     <>
