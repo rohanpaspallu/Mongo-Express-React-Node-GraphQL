7605f9f8097c54e140710204cccb3a01a619943e
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-01-04 11:13:27 -0500

    Created the logic to show client info

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { GET_PROJECT } from "../queries/projectQueries";
 import Spinner from "../components/Spinner";
+import ClientInfo from "../components/ClientInfo";
 
 const Project = () => {
   const { id } = useParams();
@@ -23,6 +24,7 @@ const Project = () => {
           <p>{data.project.description}</p>
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
+          <ClientInfo client={data.project.client} />
         </div>
       )}
     </>
