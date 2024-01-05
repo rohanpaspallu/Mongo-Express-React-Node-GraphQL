4c23939983ecc9ef530c785a89df43d8538c2cc8
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2024-01-05 07:41:29 -0500

    Created initial mutation for DELETE_PROJECT and created a button for the same

---
+++ client/src/components/DeleteProjectButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+
+const DeleteProjectButton = ({ projectId }) => {
+  return <div>DeleteProjectButton</div>;
+};
+
+export default DeleteProjectButton;
